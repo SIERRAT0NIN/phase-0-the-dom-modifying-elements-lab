afcb2294411fe7aa357f3d746e7ce0e10ff706ee
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-07-27 18:48:41 -0700

    elements_lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const mainElement = document.querySelector("main#main");
+if (mainElement) {
+  mainElement.remove();
+}
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Alberto is the champion"; // Replace YOUR-NAME with your actual name
